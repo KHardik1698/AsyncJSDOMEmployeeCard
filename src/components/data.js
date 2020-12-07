c0d22364cc9157535664693cf5091f65c4da8480
KHardik1698 <hardik.kundalia@contentstack.com>
2020-12-07 18:36:20 +0530

    Added data.js which will use Asynchronous JS mechanism Axios to fetch data
    It will return all the Employees Data received from Hosted Url

---
+++ src/components/data.js
@@ -0,0 +1,9 @@
+const url =
+  "https://niravkpatel28.github.io/json-data-server/employees/employees.json";
+
+// immediately invoked function expression IIFE
+export const data = (async () => {
+  let response = await axios.get(url);
+  console.log(response.data);
+  return response.data;
+})();
