a63cb20804f341478e4453d370d6c8f59fb33630
KHardik1698 <hardik.kundalia@contentstack.com>
2020-12-07 18:58:29 +0530

    Added employeeDiv.js which will create Employee Card for all the Employees from data.js
    It will create a new Employee Card for each Employee

---
+++ src/components/employeeDiv.js
@@ -0,0 +1,13 @@
+export const renderEmployeeCard = (employee) => {
+  let employeeCard = document.createElement("div");
+  employeeCard.innerHTML = `
+    <div class="employee-image">
+      <img src="${employee.imageUrl}" alt="" />
+          <!-- Or background img to employee image div -->
+    </div>
+    <p id="emp${employee.id}">${employee.firstName} ${employee.lastName}</p>
+    <p>${employee.company}</p>
+    `;
+  employeeCard.className = "employee-card";
+  return employeeCard;
+};
